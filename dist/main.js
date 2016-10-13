1048729bfd4cb3f5345fdb8c3eae296460d34858
Peter Hsu <sysu.peter.hsu@gmail.com>
2016-10-14 01:45:18 +0800

    first commit

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var greet_1 = require("./greet");
+// function hello(compiler: string) {
+//     console.log(`Hello from ${compiler}`);
+// }
+// hello("TypeScript");
+console.log(greet_1.sayHello("TypeScript"));
